5c440fdad6e38d7fcf8e6f87ce6fabe0a2fa7dd5
Marat Gaptullin <tdk1243@mail.ru>
2018-10-12 01:08:59 +0300

    slava site beta

---
+++ altstroi/send_mail.php
@@ -0,0 +1,19 @@
+﻿<?php
+$mailto = "dax4@yandex.ru";
+$mailto2 = "slavagavrilov08101997@mail.ru";
+$mailto3 = "krashenevav@mail.ru";
+$charset = "utf-8";
+$content = "text/html";
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: $content  charset=$charset\r\n";
+$headers .= "Date: ".date("Y-m-d (H:i:s)",time())."\r\n";
+$headers .= "From: альт-строй.рф - Заявка <альт-строй.рф - Заявка>\r\n";
+$subject = "Заявка альт-строй.рф";
+$from = "-fadmin@альт-строй.рф";
+
+$message = "Заявка<br>Имя: ".$_POST["name"]."<br>Номер: ".$_POST["phone"];
+
+mail("$mailto","$subject","$message","$headers","$from");
+mail("$mailto2","$subject","$message","$headers","$from");
+mail("$mailto3","$subject","$message","$headers","$from");
+?>
---
+++ slavas/send_mail.php
@@ -0,0 +1,17 @@
+﻿<?php
+$mailto = "dax4@yandex.ru";
+$mailto2 = "slavagavrilov08101997@mail.ru";
+$charset = "utf-8";
+$content = "text/html";
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: $content  charset=$charset\r\n";
+$headers .= "Date: ".date("Y-m-d (H:i:s)",time())."\r\n";
+$headers .= "From: aplan-digital.ru - Заявка <aplan-digital.ru - Заявка>\r\n";
+$subject = "Заявка aplan-digital.ru";
+$from = "-fadmin@aplan-digital.ru";
+
+$message = "Заявка<br>Имя: ".$_POST["name"]."<br>Номер: ".$_POST["phone"]."<br>Email: ".$_POST["email"]."<br>Сообщение: ".$_POST["task"];
+
+mail("$mailto","$subject","$message","$headers","$from");
+mail("$mailto2","$subject","$message","$headers","$from");
+?>
